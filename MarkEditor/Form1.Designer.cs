9f7bce03c6ada3ae8db3be838b7173c2f3afdcb3
korogi24 <241575737+korogi24@users.noreply.github.com>
2026-08-28 06:38:40 +0900

    ファイルタブの追加

--- MarkEditor/Form1.Designer.cs
+++ MarkEditor/Form1.Designer.cs
@@ -29,15 +29,84 @@
         private void InitializeComponent()
         {
             menuStrip = new MenuStrip();
+            ファイルToolStripMenuItem = new ToolStripMenuItem();
+            新規作成ToolStripMenuItem = new ToolStripMenuItem();
+            開くToolStripMenuItem = new ToolStripMenuItem();
+            toolStripSeparator1 = new ToolStripSeparator();
+            保存ToolStripMenuItem = new ToolStripMenuItem();
+            名前をつけて保存ToolStripMenuItem = new ToolStripMenuItem();
+            toolStripSeparator2 = new ToolStripSeparator();
+            オプションToolStripMenuItem = new ToolStripMenuItem();
+            終了ToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip.SuspendLayout();
             SuspendLayout();
             // 
             // menuStrip
             // 
+            menuStrip.Items.AddRange(new ToolStripItem[] { ファイルToolStripMenuItem });
             menuStrip.Location = new Point(0, 0);
             menuStrip.Name = "menuStrip";
             menuStrip.Size = new Size(800, 24);
             menuStrip.TabIndex = 0;
             menuStrip.Text = "menuStrip1";
+            // 
+            // ファイルToolStripMenuItem
+            // 
+            ファイルToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { 新規作成ToolStripMenuItem, 開くToolStripMenuItem, toolStripSeparator1, 保存ToolStripMenuItem, 名前をつけて保存ToolStripMenuItem, toolStripSeparator2, オプションToolStripMenuItem, 終了ToolStripMenuItem });
+            ファイルToolStripMenuItem.Name = "ファイルToolStripMenuItem";
+            ファイルToolStripMenuItem.Size = new Size(53, 20);
+            ファイルToolStripMenuItem.Text = "ファイル";
+            // 
+            // 新規作成ToolStripMenuItem
+            // 
+            新規作成ToolStripMenuItem.Image = Properties.Resources.AddDocument;
+            新規作成ToolStripMenuItem.Name = "新規作成ToolStripMenuItem";
+            新規作成ToolStripMenuItem.Size = new Size(180, 22);
+            新規作成ToolStripMenuItem.Text = "新規作成";
+            // 
+            // 開くToolStripMenuItem
+            // 
+            開くToolStripMenuItem.Image = Properties.Resources.OpenFile;
+            開くToolStripMenuItem.Name = "開くToolStripMenuItem";
+            開くToolStripMenuItem.Size = new Size(180, 22);
+            開くToolStripMenuItem.Text = "開く";
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(177, 6);
+            // 
+            // 保存ToolStripMenuItem
+            // 
+            保存ToolStripMenuItem.Image = Properties.Resources.Save;
+            保存ToolStripMenuItem.Name = "保存ToolStripMenuItem";
+            保存ToolStripMenuItem.Size = new Size(180, 22);
+            保存ToolStripMenuItem.Text = "保存";
+            // 
+            // 名前をつけて保存ToolStripMenuItem
+            // 
+            名前をつけて保存ToolStripMenuItem.Image = Properties.Resources.SaveAs;
+            名前をつけて保存ToolStripMenuItem.Name = "名前をつけて保存ToolStripMenuItem";
+            名前をつけて保存ToolStripMenuItem.Size = new Size(180, 22);
+            名前をつけて保存ToolStripMenuItem.Text = "名前をつけて保存";
+            // 
+            // toolStripSeparator2
+            // 
+            toolStripSeparator2.Name = "toolStripSeparator2";
+            toolStripSeparator2.Size = new Size(177, 6);
+            // 
+            // オプションToolStripMenuItem
+            // 
+            オプションToolStripMenuItem.Image = Properties.Resources.Settings;
+            オプションToolStripMenuItem.Name = "オプションToolStripMenuItem";
+            オプションToolStripMenuItem.Size = new Size(180, 22);
+            オプションToolStripMenuItem.Text = "オプション";
+            // 
+            // 終了ToolStripMenuItem
+            // 
+            終了ToolStripMenuItem.Name = "終了ToolStripMenuItem";
+            終了ToolStripMenuItem.Size = new Size(180, 22);
+            終了ToolStripMenuItem.Text = "終了";
             // 
             // Form1
             // 
@@ -48,6 +117,8 @@
             MainMenuStrip = menuStrip;
             Name = "Form1";
             Text = "Form1";
+            menuStrip.ResumeLayout(false);
+            menuStrip.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -55,5 +126,14 @@
         #endregion
 
         private MenuStrip menuStrip;
+        private ToolStripMenuItem ファイルToolStripMenuItem;
+        private ToolStripMenuItem 新規作成ToolStripMenuItem;
+        private ToolStripMenuItem 開くToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripMenuItem 保存ToolStripMenuItem;
+        private ToolStripMenuItem 名前をつけて保存ToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator2;
+        private ToolStripMenuItem オプションToolStripMenuItem;
+        private ToolStripMenuItem 終了ToolStripMenuItem;
     }
 }
