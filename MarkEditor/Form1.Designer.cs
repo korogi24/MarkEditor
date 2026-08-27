6df944994d3143881ec65e4f414effdd718bec12
korogi24 <241575737+korogi24@users.noreply.github.com>
2026-08-28 06:30:08 +0900

    プロジェクトを一度初期化

--- MarkEditor/Form1.Designer.cs
+++ MarkEditor/Form1.Designer.cs
@@ -28,78 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            menuStrip = new MenuStrip();
-            toolStrip = new ToolStrip();
-            splitContainer = new SplitContainer();
-            EditBox = new TextBox();
-            ((System.ComponentModel.ISupportInitialize)splitContainer).BeginInit();
-            splitContainer.Panel1.SuspendLayout();
-            splitContainer.SuspendLayout();
-            SuspendLayout();
-            // 
-            // menuStrip
-            // 
-            menuStrip.Location = new Point(0, 0);
-            menuStrip.Name = "menuStrip";
-            menuStrip.Size = new Size(800, 24);
-            menuStrip.TabIndex = 0;
-            menuStrip.Text = "menuStrip1";
-            // 
-            // toolStrip
-            // 
-            toolStrip.Location = new Point(0, 24);
-            toolStrip.Name = "toolStrip";
-            toolStrip.Size = new Size(800, 25);
-            toolStrip.TabIndex = 1;
-            toolStrip.Text = "toolStrip1";
-            // 
-            // splitContainer
-            // 
-            splitContainer.Dock = DockStyle.Fill;
-            splitContainer.Location = new Point(0, 49);
-            splitContainer.Name = "splitContainer";
-            // 
-            // splitContainer.Panel1
-            // 
-            splitContainer.Panel1.Controls.Add(EditBox);
-            splitContainer.Size = new Size(800, 401);
-            splitContainer.SplitterDistance = 400;
-            splitContainer.TabIndex = 2;
-            // 
-            // EditBox
-            // 
-            EditBox.BorderStyle = BorderStyle.None;
-            EditBox.Dock = DockStyle.Fill;
-            EditBox.Location = new Point(0, 0);
-            EditBox.Multiline = true;
-            EditBox.Name = "EditBox";
-            EditBox.Size = new Size(400, 401);
-            EditBox.TabIndex = 0;
-            // 
-            // Form1
-            // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            components = new System.ComponentModel.Container();
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(splitContainer);
-            Controls.Add(toolStrip);
-            Controls.Add(menuStrip);
-            MainMenuStrip = menuStrip;
-            Name = "Form1";
             Text = "Form1";
-            splitContainer.Panel1.ResumeLayout(false);
-            splitContainer.Panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)splitContainer).EndInit();
-            splitContainer.ResumeLayout(false);
-            ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-
-        private MenuStrip menuStrip;
-        private ToolStrip toolStrip;
-        private SplitContainer splitContainer;
-        private TextBox EditBox;
     }
 }
